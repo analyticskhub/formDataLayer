6b6641ffd19654062c82b1f232240076fdce5c77
Anil Umachigi <anilv13@outlook.com>
2017-01-02 21:11:55 +1100

    added quote and welcome event

--- oneclick_datalayer.js
+++ oneclick_datalayer.js
@@ -1,3 +1,30 @@
+/*
+ONECLICK APPLICATION DATALAYER CONVENTION
+*/
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        one click application welcome details
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+digitalData = {
+    "siteBrand": "wbc",
+    "siteName": "oneclick",
+    "siteSection": "cao",
+    "siteSubSection": "",
+    "siteEnv": "prod",
+    "siteDomain": "www.westpac.com.au",
+    "siteExperience": "mob",
+    "formName": "Cons CC",
+    "formType": "checkurl",
+    "formIsSTP": "true",
+    "formVariant": "na",
+    "newFormName": "cc",
+    "journeyType": "pub",
+    "trackOnce": "true",
+    "pageName": "welcome",
+    "pageStep": "welcome",
+    "pageType": "application"
+}
+
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
         one click application start details
 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
@@ -17,8 +44,8 @@ digitalData = {
     "journeyType": "pub",
     "accountType": "na",
     "trackOnce": "true",
-    "pageName": "welcome",
-    //"eventKey" : "mob:wbc_oregon_app_cc_thank-you",
+    "pageName": "your-details",
+    //"eventKey" : "mob:wbc_oregon_app_cc_your-details",
     "pageStep": "start",
     "pageType": "application",
     "productID": [{
@@ -51,6 +78,65 @@ digitalData = {
     "trackOnce": "true",
     "pageName": "your-details",
     "eventKey": "mob:wbc_oneclick_app_cc_your-details",
+    "pageType": "application"
+}
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        one click application save details
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+digitalData = {
+    "siteBrand": "wbc",
+    "siteName": "oneclick",
+    "siteSection": "cao",
+    "siteSubSection": "",
+    "siteEnv": "prod",
+    "siteDomain": "www.westpac.com.au",
+    "siteExperience": "mob",
+    "formName": "Cons CC",
+    "formType": "checkurl",
+    "formIsSTP": "true",
+    "formVariant": "na",
+    "newFormName": "cc",
+    "journeyType": "pub",
+    "accountType": "na",
+    "trackOnce": "true",
+    "pageName": "save-application",
+    "eventKey" : "mob:wbc_oneclick_app_cc_thank-you",
+    "pageStep": "save",
+    "pageType": "application",
+    "productID": [{
+        "prod": "WBC03PL005",
+        "qty": "1",
+        "primaryProd": "true"
+    }],
+    "appReference": [{
+        "prod": "WBC03PL005",
+        "Id": "1"
+    }
+    ]
+}
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        one click application retrive details
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+digitalData = {
+    "siteBrand": "wbc",
+    "siteName": "oneclick",
+    "siteSection": "cao",
+    "siteSubSection": "",
+    "siteEnv": "prod",
+    "siteDomain": "www.westpac.com.au",
+    "siteExperience": "mob",
+    "formName": "Cons CC",
+    "formType": "checkurl",
+    "formIsSTP": "true",
+    "formVariant": "na",
+    "newFormName": "cc",
+    "journeyType": "pub",
+    "accountType": "na",
+    "trackOnce": "true",
+    "pageName": "retrive-application",
+    "pageStep": "retrive",
     "pageType": "application"
 }
 
@@ -118,4 +204,136 @@ digitalData = {
         "Id": "1"
     }
     ]
+}
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+/*
+ONECLICK QUOTE DATALAYER CONVENTION
+*/
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        one click quote start details
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+digitalData = {
+    "siteBrand": "wbc",
+    "siteName": "oneclick",
+    "siteSection": "cao",
+    "siteSubSection": "",
+    "siteEnv": "prod",
+    "siteDomain": "www.westpac.com.au",
+    "siteExperience": "mob",
+    "formName": "Cons CC",
+    "formType": "checkurl",
+    "formIsSTP": "true",
+    "formVariant": "na",
+    "newFormName": "in",
+    "journeyType": "pub",
+    "accountType": "na",
+    "trackOnce": "true",
+    "pageName": "welcome",
+    "pageStep": "start",
+    "pageType": "quote"
+}
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        one click quote intermediate details
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+digitalData = {
+    "siteBrand": "wbc",
+    "siteName": "oneclick",
+    "siteSection": "cao",
+    "siteSubSection": "",
+    "siteEnv": "prod",
+    "siteDomain": "www.westpac.com.au",
+    "siteExperience": "mob",
+    "formName": "Cons CC",
+    "formType": "checkurl",
+    "formIsSTP": "true",
+    "formVariant": "na",
+    "newFormName": "in",
+    "journeyType": "pub",
+    "accountType": "na",
+    "trackOnce": "true",
+    "pageName": "welcome",
+    "pageType": "quote"
+}
+
+
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        one click quote save details
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+digitalData = {
+    "siteBrand": "wbc",
+    "siteName": "oneclick",
+    "siteSection": "cao",
+    "siteSubSection": "",
+    "siteEnv": "prod",
+    "siteDomain": "www.westpac.com.au",
+    "siteExperience": "mob",
+    "formName": "Cons CC",
+    "formType": "checkurl",
+    "formIsSTP": "true",
+    "formVariant": "na",
+    "newFormName": "in",
+    "journeyType": "pub",
+    "accountType": "na",
+    "trackOnce": "true",
+    "pageName": "welcome",
+    "pageStep": "save",
+    "pageType": "quote"
+}
+
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        one click quote retrive details
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+digitalData = {
+    "siteBrand": "wbc",
+    "siteName": "oneclick",
+    "siteSection": "cao",
+    "siteSubSection": "",
+    "siteEnv": "prod",
+    "siteDomain": "www.westpac.com.au",
+    "siteExperience": "mob",
+    "formName": "Cons CC",
+    "formType": "checkurl",
+    "formIsSTP": "true",
+    "formVariant": "na",
+    "newFormName": "in",
+    "journeyType": "pub",
+    "accountType": "na",
+    "trackOnce": "true",
+    "pageName": "welcome",
+    "pageStep": "retrive",
+    "pageType": "quote"
+}
+
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        one click quote complete details
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+digitalData = {
+    "siteBrand": "wbc",
+    "siteName": "oneclick",
+    "siteSection": "cao",
+    "siteSubSection": "",
+    "siteEnv": "prod",
+    "siteDomain": "www.westpac.com.au",
+    "siteExperience": "mob",
+    "formName": "Cons CC",
+    "formType": "checkurl",
+    "formIsSTP": "true",
+    "formVariant": "na",
+    "newFormName": "in",
+    "journeyType": "pub",
+    "accountType": "na",
+    "trackOnce": "true",
+    "pageName": "welcome",
+    "pageStep": "complete",
+    "pageType": "quote"
 }
